f01e2a904a220b13705f766acd9a94bb6fb55c6c
littlekingwsn <149007238+littlekingwsn@users.noreply.github.com>
2023-10-25 11:26:47 -0700

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -22,7 +22,7 @@ suite('Unit Tests', function () {
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
+      assert.isTrue(true, 'true is true');
       assert.fail(!!'double negation', 'Double negation of a truthy value is true');
       assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
